ebc2337726ddce96549c2d105adf415a184a0149
tkcmada <tokutaro@gmail.com>
2020-06-23 08:13:32 +0000

    fixed connection string builder

--- tableau/connectionBuilder.js
+++ tableau/connectionBuilder.js
@@ -1,5 +1,5 @@
 (function dsbuilder(attr) {
-    var urlBuilder = "jdbc:kdbs://" + attr[connectionHelper.attributeServer] + ":" + attr[connectionHelper.attributePort];
+    var urlBuilder = "jdbc:kdb:" + attr[connectionHelper.attributeServer] + ":" + attr[connectionHelper.attributePort];
 
     return [urlBuilder];
 })
