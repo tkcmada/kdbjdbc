27ebe326d01e9f8715ef4f1f53e54690dd9ab9ce
tkcmada <tokutaro@gmail.com>
2020-06-24 13:43:20 +0000

    update zip

--- packaging.sh
+++ packaging.sh
@@ -2,6 +2,6 @@
 
 #mvn dependency:copy-dependencies -DoutputDirectory=lib
 mvn install -DskipTests
-cp -p target/kdbjdbc*.jar lib/
+cp -p target/kdbjdbc*.jar lib/kdb_jdbc.jar
 #tar zcvf kdbjdbc.tar.gz lib
 zip -r kdbjdbc.zip lib/
