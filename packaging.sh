20543ab93aa0772c29f218380fcef3a32de1edb3
tkcmada <tokutaro@gmail.com>
2020-06-29 05:51:14 +0000

    add tableau_connectors.zip

--- packaging.sh
+++ packaging.sh
@@ -5,6 +5,7 @@ mvn clean install -DskipTests
 cp -p target/kdbjdbc*.jar lib/kdb_jdbc.jar
 #tar zcvf kdbjdbc.tar.gz lib
 #zip -r kdbjdbc.zip lib/
+zip -r tableau_connectors.zip tableau_connectors README.md
 python base64encode.py lib/kdb_jdbc.jar > kdb_jdbc.jar.base
-aws s3 cp kdbjdbc.zip s3://shimada-tmp/
-aws s3 cp lib/kdb_jdbc.jar s3://shimada-tmp/
+aws s3 cp tableau_connectors.zip s3://shimada-tmp/
+aws s3 cp kdb_jdbc.jar.base s3://shimada-tmp/
