ff11870bfb31d162ceeafacba6e1e8787ea22373
tkcmada <tokutaro@gmail.com>
2020-07-03 08:05:35 +0000

    remove sslmode property

--- tableau_connectors/postgres_jdbc/connectionProperties.js
+++ tableau_connectors/postgres_jdbc/connectionProperties.js
@@ -3,10 +3,6 @@
     props["user"] = attr[connectionHelper.attributeUsername];
     props["password"] = attr[connectionHelper.attributePassword];
 props["logdir"] = "c:\\users\\Administrator";
-    if (attr[connectionHelper.attributeSSLMode] == "require") {
-        props["ssl"] = "true";
-        props["sslmode"] = "require";
-    }
 
     return props;
 })
