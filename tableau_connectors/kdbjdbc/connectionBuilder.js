abac692117612dbc243599c2108415d153d6d797
tkcmada <tokutaro@gmail.com>
2020-08-14 07:47:24 +0000

    add log level prompt

--- tableau_connectors/kdbjdbc/connectionBuilder.js
+++ tableau_connectors/kdbjdbc/connectionBuilder.js
@@ -1,6 +1,9 @@
 (function dsbuilder(attr) {
 //    var urlBuilder = "jdbc:postgresql://" + attr[connectionHelper.attributeServer] + ":" + attr[connectionHelper.attributePort] + "/" + attr[connectionHelper.attributeDatabase] + "?";
-    var urlBuilder = "jdbc:log:jdbc:kdb:" + attr[connectionHelper.attributeServer] + ":" + attr[connectionHelper.attributePort];
-
+    var urlBuilder = "jdbc:kdb:" + attr[connectionHelper.attributeServer] + ":" + attr[connectionHelper.attributePort];
+    var loglevel = attr[connectionHelper.attributeVendor1];
+    if(loglevel == "true") {
+      urlBuilder = "jdbc:log:" + urlBuilder;
+    }
     return [urlBuilder];
 })
