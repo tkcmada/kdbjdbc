f3fd74ddecc42819358362916b3a211f8ed3ff7e
tkcmada <tokutaro@gmail.com>
2021-01-14 07:57:34 +0000

    removed unncessari files

--- package-maven-repo.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-tar zcvf repo.tar.gz ~/.m2/repository
-
--- packaging.sh
+++ packaging.sh
@@ -1,14 +1,11 @@
 #!/bin/sh
 
-#mvn dependency:copy-dependencies -DoutputDirectory=lib
 date > src/main/resources/META-INF/version.txt
 mvn clean install -DskipTests
 cp -p target/kdbjdbc*.jar lib/kdb_jdbc.jar
-#tar zcvf kdbjdbc.tar.gz lib
-#zip -r kdbjdbc.zip lib/
 rm -f  tableau_connectors.zip
 zip -r tableau_connectors.zip tableau_connectors README.md
-python base64encode.py lib/kdb_jdbc.jar > kdb_jdbc.jar.base
 aws s3 cp tableau_connectors.zip s3://shimada-tmp/
-aws s3 cp kdb_jdbc.jar.base s3://shimada-tmp/
-aws s3 cp lib/kdb_jdbc.jar s3://shimada-tmp/
+aws s3 cp lib/kdb_jdbc.jar       s3://shimada-tmp/
+#python base64encode.py lib/kdb_jdbc.jar > kdb_jdbc.jar.base
+#aws s3 cp kdb_jdbc.jar.base     s3://shimada-tmp/
